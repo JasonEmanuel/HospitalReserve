6f04a39321c3eda37e1d5b38fc6d4ed57da6a0cc
Jason <jemanuel@student.ciputra.ac.id>
2024-05-29 14:10:33 +0700

    Initial Commit

---
+++ RegisRumahSakit/RegisRumahSakitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegisRumahSakitApp.swift
+//  RegisRumahSakit
+//
+//  Created by Jason Emanuel on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct RegisRumahSakitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
