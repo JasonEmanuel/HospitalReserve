ac47dcd2081f075be7483c6ca3305162b43ef74e
Jason <jemanuel@student.ciputra.ac.id>
2024-05-30 13:46:45 +0700

    form

---
+++ RegisRumahSakit/ResultView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultView.swift
+//  RegisRumahSakit
+//
+//  Created by Jason Emanuel on 29/05/24.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultView()
+}
